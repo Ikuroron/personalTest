492bfadb402b098056b042472abf1cc22ef94698
的場郁美 <>
2020-06-05 17:59:54 +0900

    完さんヘルプ

--- result.php
+++ result.php
@@ -2,4 +2,16 @@
 
 var_dump($_POST);
 
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+
+$total = $q1 + $q2 + $q3;
+
+echo $total;
+
+if (8 <= $total && $total <= 12) {
+  echo "G";
+}
+
 ?>
